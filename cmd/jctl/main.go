79fda7222787808232d4c6a8d5e4cabf1e57532b
Jakub Jarosz <jakub@jarosz.dev>
2022-07-04 20:05:13 +0100

    Initial commit

---
+++ cmd/jctl/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/qba73/jenkins-go"
+
+func main() {
+	jenkins.RunCLI()
+}
--- jenkins.go
+++ jenkins.go
@@ -1,1 +1,32 @@
 package jenkins
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+const (
+	libraryVersion = "0.0.1"
+	userAgent      = "jenkins-go/" + libraryVersion
+	mediaType      = "application/json"
+)
+
+// Client manages communication with Jenkins API.
+type Client struct {
+	// HTTP client used to communicate with the Jenkins API.
+	client *http.Client
+
+	// Base URL for API requests.
+	BaseURL *url.URL
+
+	// User agent for the client.
+	UserAgent string
+
+	// Optional HTTP headers to set on every request to the API.
+	headers map[string]string
+}
+
+func RunCLI() {
+	fmt.Println("Running jctl")
+}
